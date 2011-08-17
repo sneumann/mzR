a17ca17e1fae3c2bd62180ec8e0e79f2afa334da
s.neumann <s.neumann@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-08-17 12:05:56 +0000

    Added netCDF libraries, rename convenience methods, fix Win build
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/mzR@57484 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/rnetCDF.c
+++ src/rnetCDF.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <netcdf.h>
 #include <string.h>
-#include "netCDF.h"
+#include "rnetCDF.h"
 
 void NetCDFStrError(const int *ncerr, const int *len, char *errortext[]) {
 
