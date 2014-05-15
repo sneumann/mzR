a622e23062d1a532fd8bc9cdf0cbde852f8e8222
qkou <qkou@umail.iu.edu>
2014-05-14 23:56:51 -0400

    boost

--- checkoutpwiz.sh
+++ checkoutpwiz.sh
@@ -31,7 +31,8 @@ svn co --non-recursive $BOOSTREPO .
 for DIR in smart_ptr  config config mpl detail iostreams exception function_types \
     io type_traits preprocessor format algorithm logic optional range numeric math\
     iterator function utility concept bind regex filesystem system thread container\
-    date_time lambda proto typeof fusion spirit tuple multi_index serialization archive functional integer locale ; do 
+    date_time lambda proto typeof fusion spirit tuple multi_index serialization\
+    variant archive functional integer locale ; do 
     svn co $BOOSTREPO/$DIR $DIR
 done
        
