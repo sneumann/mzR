bc82498f0853dfff10c3f38df551f97070a89aaf
Laurent <laurent.gatto@gmail.com>
2014-05-22 02:32:30 +0100

    remove pwiz support

--- R/io.R
+++ R/io.R
@@ -1,42 +1,34 @@
 openMSfile <- function(filename,
-                       backend=c("Ramp", "netCDF", "pwiz"),
+                       backend=c("Ramp", "netCDF"),
                        verbose = FALSE) {
-  if (!file.exists(filename))
-    stop("File ",filename," not found.\n")
-  filename <- path.expand(filename)
-  
-  if (tolower(backend) == "ramp") {
-    rampModule <- new( Ramp ) 
-    rampModule$open(filename, declaredOnly = TRUE)
-    if (!rampModule$OK()) {
-      stop("Unable to create valid cRamp object.")
-    }
-    return(new("mzRramp",
-               backend=rampModule,
-               fileName=filename))
-  } else if (tolower(backend) == "netcdf") { 
-    if (netCDFIsFile(filename)) {
-      ncid <- netCDFOpen(filename)
-      if (!is.null(attr(ncid, "errortext"))) {
-        stop(attr(ncid, "errortext"))
-      }
-      return(new("mzRnetCDF",
-                 backend=ncid,
-                 fileName=filename))
+    if (!file.exists(filename))
+        stop("File ",filename," not found.\n")
+    filename <- path.expand(filename)
+    
+    if (tolower(backend) == "ramp") {
+        rampModule <- new( Ramp ) 
+        rampModule$open(filename, declaredOnly = TRUE)
+        if (!rampModule$OK()) {
+            stop("Unable to create valid cRamp object.")
+        }
+        return(new("mzRramp",
+                   backend=rampModule,
+                   fileName=filename))
+    } else if (tolower(backend) == "netcdf") { 
+        if (netCDFIsFile(filename)) {
+            ncid <- netCDFOpen(filename)
+            if (!is.null(attr(ncid, "errortext"))) {
+                stop(attr(ncid, "errortext"))
+            }
+            return(new("mzRnetCDF",
+                       backend=ncid,
+                       fileName=filename))
+        } else {
+            stop("Unable to open netCDF file.")
+        }
     } else {
-      stop("Unable to open netCDF file.")
-    }
-  }else if(tolower(backend) == "pwiz"){
-      pwizModule <- new( Pwiz ) 
-	  pwizModule$open(filename)
-
-    return(new("mzRpwiz",
-               backend=pwizModule,
-               fileName=filename))
-  
-  } else {
-    stop("No valid backend", backend )
-  }  
+        stop("No valid backend", backend )
+    }  
 }
 
 
