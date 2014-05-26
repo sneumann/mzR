95a04ac3a1282fefdc7ada62a871c3a6f4f11b21
Laurent <laurent.gatto@gmail.com>
2014-05-26 10:46:53 +0100

    Steffen's patch

--- src/boost_aux/boost/utility/singleton.hpp
+++ src/boost_aux/boost/utility/singleton.hpp
@@ -22,7 +22,7 @@
 #       include <boost/thread/once.hpp>
 #   endif
 
-#   if defined(__GNUC__) && __GNUC__ >= 4
+#   if defined(__GNUC__) && __GNUC__ >= 4 && !defined(__clang__)
 #     define BOOST_UTILITY_SINGLETON_API __attribute__ ((visibility("default")))
 #     pragma GCC visibility push(hidden)
 #   else
@@ -204,7 +204,7 @@ namespace boost
 
 } // namespace boost
 
-#   if defined(__GNUC__) && __GNUC__ >= 4
+#   if defined(__GNUC__) && __GNUC__ >= 4 && !defined(__clang__)
 #     pragma GCC visibility pop
 #   endif
 
