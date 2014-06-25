38167821ffa6107223ea9cd3b6fdfc875b3b9342
s.neumann <s.neumann@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-06-25 04:58:28 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commits.
    Commit information:
    
    Commit id: a02c9a92c4d8c53858da5aa73b75bfbf85d6b682
    Commit message: add more generated files to ignore
    Committed by: Steffen Neumann
    Author Name: Steffen Neumann
    Commit date: 2014-06-25 06:56:46 +0200
    Author date: 2014-06-25 06:56:46 +0200
    
    Commit id: 39792622547ebe3a76ec1499de789076223ff98f
    Commit message: restored old ramp Interface
    Committed by: Steffen Neumann
    Author Name: Steffen Neumann
    Commit date: 2014-06-25 06:55:42 +0200
    Author date: 2014-06-25 06:55:42 +0200
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/mzR@91825 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/runit.oldramp.R
@@ -0,0 +1,63 @@
+test.oldramp.mzXML <- function() {
+    library(msdata)
+    library(mzR)
+    cdfpath <- system.file("threonine", package = "msdata")
+    filename <- list.files(cdfpath, pattern="threonine_i2_e35_pH_tree.mzXML",
+                       full.names=TRUE, recursive = TRUE)
+
+    rampid <- mzR:::rampOpen(filename)
+    if (rampid < 0)
+       stop("Could not open mzXML/mzData file")
+
+    on.exit(mzR:::rampClose(rampid))
+    rawdata <- mzR:::rampRawData(rampid)
+    mzR:::rampClose(rampid)
+}
+
+test.oldramp.mzML <- function() {
+    library(msdata)
+    library(mzR)
+    cdfpath <- system.file("microtofq", package = "msdata")
+    filename <- list.files(cdfpath, pattern="MM14.mzML",
+                       full.names=TRUE, recursive = TRUE)
+
+    rampid <- mzR:::rampOpen(filename)
+    if (rampid < 0)
+       stop("Could not open mzXML/mzData file")
+
+    on.exit(mzR:::rampClose(rampid))
+    rawdata <- mzR:::rampRawData(rampid)
+    mzR:::rampClose(rampid)
+}
+
+test.oldramp.mzData <- function() {
+    library(msdata)
+    library(mzR)
+    cdfpath <- system.file("microtofq", package = "msdata")
+    filename <- list.files(cdfpath, pattern="MM14.mzdata$",
+                       full.names=TRUE, recursive = TRUE)
+
+    rampid <- mzR:::rampOpen(filename)
+    if (rampid < 0)
+       stop("Could not open mzXML/mzData file")
+
+    on.exit(mzR:::rampClose(rampid))
+    rawdata <- mzR:::rampRawData(rampid)
+    mzR:::rampClose(rampid)
+}
+
+test.oldramp.mzData.gz <- function() {
+    library(msdata)
+    library(mzR)
+    cdfpath <- system.file("microtofq", package = "msdata")
+    filename <- list.files(cdfpath, pattern="MM14.mzdata.gz",
+                       full.names=TRUE, recursive = TRUE)
+
+    rampid <- mzR:::rampOpen(filename)
+    if (rampid < 0)
+       stop("Could not open mzXML/mzData file")
+
+    on.exit(mzR:::rampClose(rampid))
+    rawdata <- mzR:::rampRawData(rampid)
+    mzR:::rampClose(rampid)
+}
