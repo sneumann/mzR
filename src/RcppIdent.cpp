464d4e83a9e06732f7c3f1a0172a9cb2bbd53943
thirdwing <kellyhavel1989@gmail.com>
2014-06-28 17:21:01 -0400

    modified:   src/RcppIdent.cpp

--- src/RcppIdent.cpp
+++ src/RcppIdent.cpp
@@ -15,10 +15,17 @@ Rcpp::List RcppIdent::getIDInfo(  )
 {
 	provider = (mzid->provider.contactRolePtr.get()!=0?mzid->provider.contactRolePtr.get()->name():"");
     date = mzid->creationDate;
-    
+    vector<AnalysisSoftwarePtr> as = mzid->analysisSoftwareList;
+    int N = as.size();
+    Rcpp::StringVector software(N);
+    for (size_t i = 0; i < as.size(); i++)
+    {
+		software[i] = as[i]->name + " " + as[i]->version + " " + (as[i]->contactRolePtr.get()!=0?as[i]->contactRolePtr->contactPtr->name:"") ;
+    }
 	return Rcpp::List::create(
                    Rcpp::_["FileProvider"]	= provider,
-                   Rcpp::_["CreationDate"]	= date
+                   Rcpp::_["CreationDate"]	= date,
+                   Rcpp::_["software"]	= software
                );
     
 }
