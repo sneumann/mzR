9d0350c29e3c20e7ebbc6869675e59b61720fcc6
Steffen Neumann <sneumann@ipb-halle.de>
2015-02-22 00:05:31 +0100

    Add instrumentInfo() and runInfo() for CDF backend (closes #22), Add precompiled libpwiz.a to reduce compile time on Windows (closes #21), thanks to KK

--- src/rnetCDF.h
+++ src/rnetCDF.h
@@ -14,6 +14,10 @@ void NetCDFVarDouble(const int *ncid, const int *varid, double *data, int *statu
 
 void NetCDFVarInt(const int *ncid, const int *varid, int *data, int *status);
 
+void NetCDFVarText(const int *ncid, const int *varid, char **data, int *status);
+
+void NetCDFGlobalAttribute(const int *ncid, const char **attname, char **data, int *status);
+
 void NetCDFMSPoints(const int *ncid, const int *scanNumber,
                     const int *scanIndex, const int *pointNumber,
                     double *massValues, double *intensityValues, int *status);
