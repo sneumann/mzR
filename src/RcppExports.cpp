5879cb75703fc6f4bd020834585388fe1b51e143
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-12-15 10:56:00 +0000

    Modified to compile under Windows, added missing files, includes Windows specifics into Makevars
    From: Steffen Neumann <sneumann@ipb-halle.de>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/mzR@125191 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -1,9 +1,12 @@
-
 #include <Rcpp.h>
 #include "pwiz/data/msdata/Version.hpp"
 
 using namespace Rcpp;
 
+//' Get Proteowizard Version
+//' 
+//' Description
+//' Return the pwiz::msdata::Version as String
 RcppExport SEXP mzR_pwiz_version() {
 BEGIN_RCPP
     Rcpp::RObject __result;
