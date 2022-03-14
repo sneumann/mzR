68e17202e1d6fa03b3dd41452f451705d8c6e1aa
Steffen Neumann <sneumann@ipb-halle.de>
2022-03-14 11:47:04 +0100

    re-apply clang compile fix after pwiz upgrade, closes #263

--- src/pwiz/data/common/BinaryIndexStream.cpp
+++ src/pwiz/data/common/BinaryIndexStream.cpp
@@ -6,16 +6,16 @@
 //
 // Copyright 2009 Vanderbilt University - Nashville, TN 37232
 //
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 //
 
@@ -154,7 +154,7 @@ class stream_vector_const_iterator
             return (that.end_ - that.begin_) / that.value_size_;
 	}
 
-	reference operator[](difference_type _Off) const
+	reference operator[](difference_type _Off)
 	{
         next_ = _Off * value_size_;
 		return (**this);
@@ -370,7 +370,7 @@ class BinaryIndexStream::Impl
         EntryPtr entryPtr(new Entry); entryPtr->id = id;
         const stream_offset indexBegin = indexedMetadataHeaderSize_ + sizeof(streamLength_) + sizeof(maxIdLength_) + entrySize_ * size_;
         const stream_offset indexEnd = indexedMetadataHeaderSize_ + streamLength_;
-      
+
         {
             boost::mutex::scoped_lock io_lock(io_mutex);
             isPtr_->clear();
