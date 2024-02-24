5f500973666ee0cc719998ff7cc9322307e8ab94
Laurent Gatto <laurent.gatto@uclouvain.be>
2024-02-24 15:18:16 +0100

    remove mentions of mzData

--- src/RcppIdentModule.cpp
+++ src/RcppIdentModule.cpp
@@ -9,7 +9,7 @@ RCPP_MODULE(Ident)
 
     class_<RcppIdent>( "Ident" )
     .constructor("Initialises a new Rccp ident object.")
-    .method( "open", &RcppIdent::open, "Opens a mass spec file (mzXML, mzData, etc.) and creates a pwiz object" )
+    .method( "open", &RcppIdent::open, "Opens a mass spec file (mzXML, mzML, etc.) and creates a pwiz object" )
     .method( "getIDInfo", &RcppIdent::getIDInfo, "Basic information about this mzid files" )
     .method( "getPsmInfo", &RcppIdent::getPsmInfo, "Basic information about this mzid files" )
     .method( "getModInfo", &RcppIdent::getModInfo, "Modification information about this mzid files" )
